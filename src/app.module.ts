954bbd6e9d15decd6f859f8096b6ed581652998a
RaisZzz <beats.tapping@gmail.com>
2025-05-08 18:32:44 +0500

    ADD: Database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV || 'production'}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: String(process.env.DB_USER),
+      password: String(process.env.DB_PASSWORD),
+      database: String(process.env.DB_NAME),
+      autoLoadModels: true,
+      models: [],
+    }),
+  ],
+})
 export class AppModule {}
